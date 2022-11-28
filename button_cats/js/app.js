10ed91cc04cc7832ebd82717d22599bd67921b28
Jack's-Style <117783266+jacks-style@users.noreply.github.com>
2022-11-28 19:08:26 +0100

    Delete button_cats directory

--- button_cats/js/app.js
+++
@@ -1,9 +0,0 @@
-const BUTTON = document.getElementById("cat__button");
-const IMAGE = document.getElementById("cats__img");
-
-BUTTON.addEventListener("mouseover", () => {
-  IMAGE.style.display = "block";
-});
-BUTTON.addEventListener("mouseout", () => {
-  IMAGE.style.display = "none";
-});
