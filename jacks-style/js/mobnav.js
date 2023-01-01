7d78c002fd9ce1cf81b8161f9e18f188de3bc152
Jack's Style <117783266+jacks-style@users.noreply.github.com>
2023-01-01 03:57:51 +0100

    Add files via upload

---
+++ jacks-style/js/mobnav.js
@@ -0,0 +1,24 @@
+// MOBILE NAV
+const mobileNav = document.querySelector(".navimobile");
+
+function mobileMenuOpen() {
+  mobileNav.style.display = "block";
+}
+
+document.querySelector(".navimobile__item1").addEventListener("click", () => {
+  mobileNav.style.display = "none";
+});
+document.querySelector(".navimobile__item2").addEventListener("click", () => {
+  mobileNav.style.display = "none";
+});
+document.querySelector(".navimobile__item3").addEventListener("click", () => {
+  mobileNav.style.display = "none";
+});
+document.querySelector(".navimobile__item4").addEventListener("click", () => {
+  mobileNav.style.display = "none";
+});
+document
+  .querySelector(".navimobile__button--close")
+  .addEventListener("click", () => {
+    mobileNav.style.display = "none";
+  });
